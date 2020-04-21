7e9e7e04fd01746043b8efe628865ae67fb04120
baijianbiao <baijianbiao@weidian.com>
2020-04-21 11:34:03 +0800

    rpc

---
+++ src/main/java/org/example/rpc/server/registry/RegisterTest.java
@@ -0,0 +1,10 @@
+package org.example.rpc.server.registry;
+
+
+public class RegisterTest {
+    public static void main(String[] args) throws Exception {
+        RegistryCenter center = new ZKRegistryCenter();
+        center.register("com.abc.service.SomeService2", "localhost:8888");
+        System.in.read();
+    }
+}
---
+++ src/main/java/org/example/rpc/server/registry/RegistryCenter.java
@@ -0,0 +1,13 @@
+package org.example.rpc.server.registry;
+
+/**
+ * 注册规范
+ */
+public interface RegistryCenter {
+    /**
+     *
+     * @param serviceName  注册到注册中心的服务名称，一般为业务接口名
+     * @param serviceAddress  提供该服务的主机的ip:port
+     */
+    void register(String serviceName, String serviceAddress) throws Exception;
+}
